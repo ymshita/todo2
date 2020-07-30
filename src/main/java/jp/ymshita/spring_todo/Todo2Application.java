646e769ded237c4a74506a6f99e907b3f2715ff7
yyshita <yama.98904@gmail.com>
2020-07-30 15:02:38 +0900

    Initial commit

---
+++ src/main/java/jp/ymshita/spring_todo/Todo2Application.java
@@ -0,0 +1,13 @@
+package jp.ymshita.spring_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todo2Application.class, args);
+	}
+
+}
